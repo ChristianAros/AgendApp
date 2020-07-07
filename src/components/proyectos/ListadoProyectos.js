236cc8d5fc731fbbd11e3a4ca9dedc290aec01ac
Christian Aros <christianarosr@gmail.com>
2020-07-07 11:56:37 -0300

    Ocultando el form de tareas si no hay un proyecto seleccionado y eliminando proyectos

---
+++ src/components/proyectos/ListadoProyectos.js
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect } from 'react'
+import Proyecto from './Proyecto'
+import proyectoContext from '../../context/proyectos/proyectoContext'
+
+const ListadoProyectos = () => {
+
+    //extraer proyectos del state inicial
+    const proyectosContext = useContext(proyectoContext)
+    const { proyectos, obtenerProyectos } = proyectosContext
+
+    //obtener proyectos cuando carga el componente
+    useEffect(() => {
+        obtenerProyectos()
+    }, [])
+
+    //revisar si proyectos tiene contenido
+    if(proyectos.length === 0) return <p>No hay proyectos, crea uno ahora!</p>
+
+   
+   
+    return ( 
+        <ul className="listado-proyectos">
+            {proyectos.map(proyecto => (
+                <Proyecto
+                    key={proyecto.id}
+                    proyecto={proyecto}
+                 />
+            ))}
+        </ul>
+     );
+}
+ 
+export default ListadoProyectos;
